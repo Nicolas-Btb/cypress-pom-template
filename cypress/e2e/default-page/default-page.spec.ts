7d7d6dd015823bebfaeb5dcb83b1c3240b9054da
nboutouba-isagri <nboutouba@isagri.fr>
2025-03-19 15:54:48 +0100

    fake multi testing

--- cypress/e2e/default-page/default-page.spec.ts
+++ cypress/e2e/default-page/default-page.spec.ts
@@ -20,5 +20,57 @@ describe('Default Page testing', () => {
         DefaultPage.goToHome();
         UrlTools.checkUrlInclude('http://localhost:5173/');        
     });
+
+    it('should search for a product 2', () => {
+        DefaultPage.searchForProduct('Mens');
+        DefaultPage.countSearchResults(4);
+        
+        DefaultPage.clickOnSearchedProductByProductId(2);
+        //example if you don't know the product id
+        //DefaultPage.clickOnSearchedProductByProductName('Mens Cotton Jacket');
+        UrlTools.checkUrlInclude('product/3');
+
+        DefaultPage.goToHome();
+        UrlTools.checkUrlInclude('http://localhost:5173/');        
+    });
+
+    it('should search for a product 3', () => {
+        DefaultPage.searchForProduct('Mens');
+        DefaultPage.countSearchResults(4);
+        
+        DefaultPage.clickOnSearchedProductByProductId(2);
+        //example if you don't know the product id
+        //DefaultPage.clickOnSearchedProductByProductName('Mens Cotton Jacket');
+        UrlTools.checkUrlInclude('product/3');
+
+        DefaultPage.goToHome();
+        UrlTools.checkUrlInclude('http://localhost:5173/');        
+    });
+
+    it('should search for a product 4', () => {
+        DefaultPage.searchForProduct('Mens');
+        DefaultPage.countSearchResults(4);
+        
+        DefaultPage.clickOnSearchedProductByProductId(2);
+        //example if you don't know the product id
+        //DefaultPage.clickOnSearchedProductByProductName('Mens Cotton Jacket');
+        UrlTools.checkUrlInclude('product/3');
+
+        DefaultPage.goToHome();
+        UrlTools.checkUrlInclude('http://localhost:5173/');        
+    });
+
+    it('should search for a product 5', () => {
+        DefaultPage.searchForProduct('Mens');
+        DefaultPage.countSearchResults(4);
+        
+        DefaultPage.clickOnSearchedProductByProductId(2);
+        //example if you don't know the product id
+        //DefaultPage.clickOnSearchedProductByProductName('Mens Cotton Jacket');
+        UrlTools.checkUrlInclude('product/3');
+
+        DefaultPage.goToHome();
+        UrlTools.checkUrlInclude('http://localhost:5173/');        
+    });
     
 });
